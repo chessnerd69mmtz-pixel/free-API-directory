7183b3db37f231b350b9ec0e9f466e3efb40145f
Aryan Mohammed <chessnerd69mmtz@gmail.com>
2026-09-26 11:24:15 +0530

    Add data quality dashboard to navigation

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ function nav() {
     '<a href="compare.html">Compare</a>' +
     '<a href="keys.html">🔐 My API Keys</a>' +
     '<a href="changes.html">Verified / Changed</a>' +
-    '<a href="criteria.html">Browse</a>' +
+    '<a href="criteria.html">Browse</a><a href="quality.html">Data Quality</a>' +
   "</nav>";
 }
 
